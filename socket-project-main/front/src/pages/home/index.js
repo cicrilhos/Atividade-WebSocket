81befef3f634f260993e7fd2b27f0bbfcd11b687
Wynne <wynnesindor123@gmail.com>
2025-11-11 15:32:26 -0300

    oii

--- socket-project-main/front/src/pages/home/index.js
+++ socket-project-main/front/src/pages/home/index.js
@@ -78,7 +78,7 @@ export default function Login() {
                     Sair
                 </button>
             </div>
-
+            
             <div className="chat">
                 {salaAtual ? (
                     <>
